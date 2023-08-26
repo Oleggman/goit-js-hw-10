81474f6bb9070d5b8b485ed4e24f43f9cfdf88d8
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-08-27 00:16:31 +0300

    Fetch cats by options

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import {fetchBreeds} from "./js/cat-api";
+import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 
 const refs = {
   select: document.querySelector('.breed-select'),
   area: document.querySelector('.cat-info'),
 }
 
-fetchBreeds().then(addOptions);
+refs.select.style.display = "none";
+refs.select.addEventListener('change', onSelect);
+
+fetchBreeds().then(addOptions).finally(() => refs.select.style.display = "inline-block");
 
 function addOptions(data) {
   let options = "";
@@ -15,4 +18,18 @@ function addOptions(data) {
   });
 
   refs.select.innerHTML = options;
+}
+
+function onSelect(evt) {
+  fetchCatByBreed(evt.target.value).then(renderCat).catch(console.log);
+}
+
+function renderCat({ data, url }) {
+  const markup = `
+    <img src="${url}" alt="${data.name}" width="500">
+    <h2>${data.name}</h2>
+    <p>${data.description}</p>
+    <p>${data.temperament}</p>`;
+
+  refs.area.innerHTML = markup;
 }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_Vc0UBn8V9fjNTSwaOGE0sEkBZGz8ypaOzHaAKEiCjQHRRZAHnApsT3NCrSphubrd";
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const BREED_URL = 'https://api.thecatapi.com/v1/breeds';
+const CAT_URL = 'https://api.thecatapi.com/v1/images/search';
 
 function fetchBreeds() {
-  return axios('https://api.thecatapi.com/v1/breeds').then(response => response.data);
+  return axios(BREED_URL).then(response => response.data).catch(error => error.message);
 }
 
 function fetchCatByBreed(breedId) {
-  return axios(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-         .then(console.log);
+  return axios(`${CAT_URL}?breed_ids=${breedId}`).then(response => {
+    const url = response.data[0].url;
+    const data = response.data[0].breeds[0];
+    return { data, url };
+  }).catch(error => error.message);
 }
 
 export { fetchBreeds, fetchCatByBreed };
