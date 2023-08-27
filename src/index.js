fa95b1d4330bdfe0f2938b72a8eb2828ba0d0dfc
Oleh Kovalets <olegkovalets1112003@gmail.com>
2023-08-27 03:22:14 +0300

    Add custom styles

--- src/index.js
+++ src/index.js
@@ -3,33 +3,43 @@ import { fetchBreeds, fetchCatByBreed } from "./js/cat-api";
 const refs = {
   select: document.querySelector('.breed-select'),
   area: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
 }
 
-refs.select.style.display = "none";
+refs.area.style.display = "none"
 refs.select.addEventListener('change', onSelect);
 
-fetchBreeds().then(addOptions).finally(() => refs.select.style.display = "inline-block");
+fetchBreeds().then(addOptions).finally(onRenderOptions);
 
 function addOptions(data) {
-  let options = "";
-
-  data.forEach(({id, name}) => {
-    options += `<option value="${id}">${name}</option>`
-  });
+  const options = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
 
   refs.select.innerHTML = options;
 }
 
+function onRenderOptions() {
+  refs.select.classList.remove("hidden");
+  refs.loader.style.display = "none";
+}
+
 function onSelect(evt) {
+  refs.loader.style.display = "inline-block";
+  refs.area.style.display = "none";
   fetchCatByBreed(evt.target.value).then(renderCat).catch(console.log);
 }
 
 function renderCat({ data, url }) {
   const markup = `
-    <img src="${url}" alt="${data.name}" width="500">
-    <h2>${data.name}</h2>
-    <p>${data.description}</p>
-    <p>${data.temperament}</p>`;
+    <div class="cat-thumb">
+      <img class="cat-img" src="${url}" alt="${data.name}" width="500">
+    </div>
+    <div class="cat-description">
+      <h2 class="cat-name">${data.name}</h2>
+      <p class="cat-desc">${data.description}</p>
+      <p class="cat-temp">${data.temperament}</p>
+    </div>`;
 
+  refs.area.style.display = "flex";
   refs.area.innerHTML = markup;
+  refs.loader.style.display = "none";
 }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 
 axios.defaults.headers.common["x-api-key"] = "live_Vc0UBn8V9fjNTSwaOGE0sEkBZGz8ypaOzHaAKEiCjQHRRZAHnApsT3NCrSphubrd";
+
 const BREED_URL = 'https://api.thecatapi.com/v1/breeds';
 const CAT_URL = 'https://api.thecatapi.com/v1/images/search';
+const ERROR_MESSAGE = "Oops! Something went wrong! Try reloading the page\n";
 
 function fetchBreeds() {
-  return axios(BREED_URL).then(response => response.data).catch(error => error.message);
+  return axios(BREED_URL).then(response => response.data).catch(error => Notiflix.Notify.failure(ERROR_MESSAGE));
 }
 
 function fetchCatByBreed(breedId) {
@@ -13,7 +16,7 @@ function fetchCatByBreed(breedId) {
     const url = response.data[0].url;
     const data = response.data[0].breeds[0];
     return { data, url };
-  }).catch(error => error.message);
+  }).catch(error => Notiflix.Notify.failure(ERROR_MESSAGE));
 }
 
 export { fetchBreeds, fetchCatByBreed };
